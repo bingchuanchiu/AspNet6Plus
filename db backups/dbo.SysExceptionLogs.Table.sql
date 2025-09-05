5436b0783f919c759d1808749d8621604fff9a86
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-05 16:29:12 -0400

    Update and rename dbo.SystemExceptionLogs.Table.sql to dbo.SysExceptionLogs.Table.sql

---
+++ db backups/dbo.SysExceptionLogs.Table.sql
@@ -0,0 +1,23 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[SysExceptionLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SysExceptionLogs](
+	[Sys_exception_log_id] [int] IDENTITY(1,1) NOT NULL,
+	[message] [nvarchar](max) NULL,
+	[inner_exception] [nvarchar](max) NULL,
+	[stack_trace] [nvarchar](max) NULL,
+	[created_at] [datetime2](7) NULL,
+	[created_by] [int] NULL,
+ CONSTRAINT [PK_SysExceptionLogs] PRIMARY KEY CLUSTERED 
+(
+	[Sys_exception_log_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[SysExceptionLogs] ADD  CONSTRAINT [DF_SysExceptionLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
+GO
+
