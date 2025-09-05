7bbd3e5c73424b68c8fd93c2677348fe9c821f5d
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-05 16:27:36 -0400

    Update and rename dbo.SystemEmailLogs.Table.sql to dbo.SysEmailLogs.Table.sql

---
+++ db backups/dbo.SysEmailLogs.Table.sql
@@ -0,0 +1,27 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[SysEmailLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SysEmailLogs](
+	[Sys_email_log_id] [int] IDENTITY(1,1) NOT NULL,
+	[email_from] [nvarchar](150) NULL,
+	[email_to] [nvarchar](max) NULL,
+	[email_cc] [nvarchar](max) NULL,
+	[email_bcc] [nvarchar](max) NULL,
+	[email_subject] [nvarchar](150) NULL,
+	[email_body] [nvarchar](max) NULL,
+	[email_attachment] [varbinary](max) NULL,
+	[created_at] [datetime2](7) NOT NULL,
+	[created_by] [int] NOT NULL,
+ CONSTRAINT [PK_SysEmailLogs] PRIMARY KEY CLUSTERED 
+(
+	[Sys_email_log_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[SysEmailLogs] ADD  CONSTRAINT [DF_SysEmailLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
+GO
+
