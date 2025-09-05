c509bfa1a3970865d3b33b0e337a93714791dbbe
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-05 16:25:32 -0400

    Update and rename dbo.SystemCellCarriers.Table.sql to dbo.SysCellCarriers.Table.sql

---
+++ db backups/dbo.SysCellCarriers.Table.sql
@@ -0,0 +1,19 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[SysCellCarriers]    Script Date: 2/11/2025 10:30:58 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SysCellCarriers](
+	[Sys_cell_carrier_id] [int] IDENTITY(1,1) NOT NULL,
+	[cell_carrier_name] [nvarchar](150) NOT NULL,
+	[sms_gateway] [nvarchar](150) NOT NULL,
+	[sms_notifications] [bit] NULL,
+ CONSTRAINT [PK_SystemCellCarriers] PRIMARY KEY CLUSTERED 
+(
+	[Sys_cell_carrier_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
