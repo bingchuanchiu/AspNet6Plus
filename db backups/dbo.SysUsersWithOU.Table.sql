9ac5d2da105b0b4e6baae7fd048e55d175e3f357
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 10:04:26 -0400

    Update and rename dbo.SystemUsersWithOU.Table.sql to dbo.SysUsersWithOU.Table.sql

--- db backups/dbo.SysUsersWithOU.Table.sql
+++ db backups/dbo.SysUsersWithOU.Table.sql
@@ -5,8 +5,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SystemUsers](
-	[system_user_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[SysUsers](
+	[Sys_user_id] [int] IDENTITY(1,1) NOT NULL,
 	[user_type_id] [int] NULL,
 	[username] [nvarchar](100) NULL,
 	[firstname] [nvarchar](100) NULL,
@@ -23,14 +23,15 @@ CREATE TABLE [dbo].[SystemUsers](
 	[updated_at] [datetime2](7) NULL,
 	[created_by] [int] NULL,
 	[updated_by] [int] NULL,
- CONSTRAINT [PK_SystemUsers] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_SysUsers] PRIMARY KEY CLUSTERED 
 (
-	[system_user_id] ASC
+	[Sys_user_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-SET IDENTITY_INSERT [dbo].[SystemUsers] ON 
+SET IDENTITY_INSERT [dbo].[SysUsers] ON 
 
-INSERT [dbo].[SystemUsers] ([system_user_id], [user_type_id], [username], [firstname], [lastname], [emplid], [email], [email_notifications], [thumbnail], [ldap_whenChanged], [ldap_whenCreated], [is_disabled], [lockout_time], [created_at], [updated_at], [created_by], [updated_by]) VALUES (1, NULL, N'bchiu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+INSERT [dbo].[SysUsers] ([Sys_user_id], [user_type_id], [username], [firstname], [lastname], [emplid], [email], [email_notifications], [thumbnail], [ldap_whenChanged], [ldap_whenCreated], [is_disabled], [lockout_time], [created_at], [updated_at], [created_by], [updated_by]) VALUES (1, NULL, N'bchiu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
 SET IDENTITY_INSERT [dbo].[Users] OFF
 GO
+
