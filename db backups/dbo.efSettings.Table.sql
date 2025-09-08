6bb6b9188b598d303eb734accd5712ffb64b5e20
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 15:05:15 -0400

    Update and rename dbo.SysSettings.Table.sql to dbo.efSettings.Table.sql

--- db backups/dbo.efSettings.Table.sql
+++ db backups/dbo.efSettings.Table.sql
@@ -1,12 +1,12 @@
 ﻿USE [Complex]
 GO
-/****** Object:  Table [dbo].[SysSettings]    Script Date: 2/11/2025 10:30:58 AM ******/
+/****** Object:  Table [dbo].[efSettings]    Script Date: 2/11/2025 10:30:58 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SysSettings](
-	[Sys_setting_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[efSettings](
+	[ef_setting_id] [int] IDENTITY(1,1) NOT NULL,
 	[setting_name] [nvarchar](50) NULL,
 	[display_name] [nvarchar](50) NULL,
 	[value] [nvarchar](max) NULL,
@@ -14,12 +14,13 @@ CREATE TABLE [dbo].[SysSettings](
 	[updated_at] [datetime2](7) NULL,
 	[created_by] [int] NULL,
 	[updated_by] [int] NULL,
- CONSTRAINT [PK_SysSettings] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_efSettings] PRIMARY KEY CLUSTERED 
 (
-	[Sys_setting_id] ASC
+	[ef_setting_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[SysSettings] ADD  CONSTRAINT [DF_SysSettings_created_at]  DEFAULT (getdate()) FOR [created_at]
+ALTER TABLE [dbo].[efSettings] ADD  CONSTRAINT [DF_efSettings_created_at]  DEFAULT (getdate()) FOR [created_at]
 GO
+
 
