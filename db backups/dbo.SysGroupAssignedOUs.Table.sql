8a6df269e35fcc7fb1ec506192dae79c3b33545e
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 09:48:19 -0400

    Update dbo.SysGroupAssignedOUs.Table.sql

--- db backups/dbo.SysGroupAssignedOUs.Table.sql
+++ db backups/dbo.SysGroupAssignedOUs.Table.sql
@@ -6,13 +6,13 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[SysGroupAssignedOUs](
-	[Sys_group_assigned_Sys_ou_id] [int] IDENTITY(1,1) NOT NULL,
+	[Sys_group_assigned_ou_id] [int] IDENTITY(1,1) NOT NULL,
 	[Sys_group_id] [int] NOT NULL,
 	[Sys_ou_id] [int] NOT NULL,
 	[active] [bit] NULL,
  CONSTRAINT [PK_UserAssignedFIMGroups] PRIMARY KEY CLUSTERED 
 (
-	[Sys_group_assigned_Sys_ou_id] ASC
+	[Sys_group_assigned_ou_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -21,4 +21,5 @@ REFERENCES [dbo].[SysGroups] ([Sys_group_id])
 GO
 ALTER TABLE [dbo].[SysGroupAssignedOUs] CHECK CONSTRAINT [FK_SysGroupAssignedOUs_Groups]
 GO
+
 
