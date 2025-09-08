3be4ede28815f4869174836a3e66f3728b3ec9e2
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 14:53:05 -0400

    Update and rename dbo.SysUsers.Table.sql to dbo.efUsers.Table.sql

--- db backups/dbo.efUsers.Table.sql
+++ db backups/dbo.efUsers.Table.sql
@@ -1,12 +1,12 @@
 ﻿USE [Complex]
 GO
-/****** Object:  Table [dbo].[SysUsers]    Script Date: 2/11/2025 10:30:58 AM ******/
+/****** Object:  Table [dbo].[efUsers]    Script Date: 2/11/2025 10:30:58 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SysUsers](
-	[Sys_user_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[efUsers](
+	[ef_user_id] [int] IDENTITY(1,1) NOT NULL,
 	[user_type_id] [int] NULL,
 	[username] [nvarchar](100) NULL,
 	[firstname] [nvarchar](100) NULL,
@@ -20,15 +20,16 @@ CREATE TABLE [dbo].[SysUsers](
 	[updated_at] [datetime2](7) NULL,
 	[created_by] [int] NULL,
 	[updated_by] [int] NULL,
- CONSTRAINT [PK_SysUsers] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_efUsers] PRIMARY KEY CLUSTERED 
 (
-	[Sys_user_id] ASC
+	[ef_user_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-SET IDENTITY_INSERT [dbo].[SysUsers] ON 
+SET IDENTITY_INSERT [dbo].[efUsers] ON 
 
-INSERT [dbo].[SysUsers] ([Sys_user_id], [user_type_id], [username], [firstname], [lastname], [emplid], [email], [email_notifications], [thumbnail], [is_disabled], [created_at], [updated_at], [created_by], [updated_by]) VALUES (1, NULL, N'bchiu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-SET IDENTITY_INSERT [dbo].[SysUsers] OFF
+INSERT [dbo].[efUsers] ([ef_user_id], [user_type_id], [username], [firstname], [lastname], [emplid], [email], [email_notifications], [thumbnail], [is_disabled], [created_at], [updated_at], [created_by], [updated_by]) VALUES (1, NULL, N'bchiu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[efUsers] OFF
 GO
+
 
