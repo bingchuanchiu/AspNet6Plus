4cea3e632de482819733cb7ab81928b4dbdc3e4e
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 15:19:37 -0400

    Update and rename dbo.SysEmailLogs.Table.sql to dbo.efEmailLogs.Table.sql

--- db backups/dbo.efEmailLogs.Table.sql
+++ db backups/dbo.efEmailLogs.Table.sql
@@ -1,12 +1,12 @@
 ﻿USE [Complex]
 GO
-/****** Object:  Table [dbo].[SysEmailLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
+/****** Object:  Table [dbo].[efEmailLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SysEmailLogs](
-	[Sys_email_log_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[efEmailLogs](
+	[ef_email_log_id] [int] IDENTITY(1,1) NOT NULL,
 	[email_from] [nvarchar](150) NULL,
 	[email_to] [nvarchar](max) NULL,
 	[email_cc] [nvarchar](max) NULL,
@@ -16,12 +16,13 @@ CREATE TABLE [dbo].[SysEmailLogs](
 	[email_attachment] [varbinary](max) NULL,
 	[created_at] [datetime2](7) NOT NULL,
 	[created_by] [int] NOT NULL,
- CONSTRAINT [PK_SysEmailLogs] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_efEmailLogs] PRIMARY KEY CLUSTERED 
 (
-	[Sys_email_log_id] ASC
+	[ef_email_log_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[SysEmailLogs] ADD  CONSTRAINT [DF_SysEmailLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
+ALTER TABLE [dbo].[efEmailLogs] ADD  CONSTRAINT [DF_efEmailLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
 GO
+
 
