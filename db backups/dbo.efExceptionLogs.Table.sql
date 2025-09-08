357a3716320f0b06cc9ba1f3f6fc6d45d24f4506
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 15:19:15 -0400

    Update and rename dbo.SysExceptionLogs.Table.sql to dbo.efExceptionLogs.Table.sql

--- db backups/dbo.efExceptionLogs.Table.sql
+++ db backups/dbo.efExceptionLogs.Table.sql
@@ -1,23 +1,24 @@
 ﻿USE [Complex]
 GO
-/****** Object:  Table [dbo].[SysExceptionLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
+/****** Object:  Table [dbo].[efExceptionLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SysExceptionLogs](
-	[Sys_exception_log_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[efExceptionLogs](
+	[ef_exception_log_id] [int] IDENTITY(1,1) NOT NULL,
 	[message] [nvarchar](max) NULL,
 	[inner_exception] [nvarchar](max) NULL,
 	[stack_trace] [nvarchar](max) NULL,
 	[created_at] [datetime2](7) NULL,
 	[created_by] [int] NULL,
- CONSTRAINT [PK_SysExceptionLogs] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_efExceptionLogs] PRIMARY KEY CLUSTERED 
 (
-	[Sys_exception_log_id] ASC
+	[ef_exception_log_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[SysExceptionLogs] ADD  CONSTRAINT [DF_SysExceptionLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
+ALTER TABLE [dbo].[efExceptionLogs] ADD  CONSTRAINT [DF_efExceptionLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
 GO
+
 
