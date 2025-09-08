2b90b8593fb7845e21f44002877d0057ba4d6ec2
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 15:19:58 -0400

    Update and rename dbo.SysDBLogs.Table.sql to dbo.efDBLogs.Table.sql

--- db backups/dbo.efDBLogs.Table.sql
+++ db backups/dbo.efDBLogs.Table.sql
@@ -1,12 +1,12 @@
 ﻿USE [Complex]
 GO
-/****** Object:  Table [dbo].[SysDBLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
+/****** Object:  Table [dbo].[efDBLogs]    Script Date: 2/11/2025 10:30:58 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[SysDBLogs](
-	[Sys_db_log_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[efDBLogs](
+	[ef_db_log_id] [int] IDENTITY(1,1) NOT NULL,
 	[table_name] [nvarchar](max) NULL,
 	[column_name] [nvarchar](max) NULL,
 	[column_type] [nvarchar](max) NULL,
@@ -14,13 +14,14 @@ CREATE TABLE [dbo].[SysDBLogs](
 	[column_new_value] [nvarchar](max) NULL,
 	[created_at] [datetime2](7) NULL,
 	[created_by] [int] NULL,
- CONSTRAINT [PK_SysDBLogs] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_efDBLogs] PRIMARY KEY CLUSTERED 
 (
-	[Sys_db_log_id] ASC
+	[ef_db_log_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[SysDBLogs] ADD  CONSTRAINT [DF_SysDBLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
+ALTER TABLE [dbo].[efDBLogs] ADD  CONSTRAINT [DF_efDBLogs_created_at]  DEFAULT (getdate()) FOR [created_at]
 GO
+
 
 
