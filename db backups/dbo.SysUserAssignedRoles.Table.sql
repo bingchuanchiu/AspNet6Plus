40ffbc28ebfe5ba7c2220e3c93782805e4bfdb87
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 10:02:54 -0400

    Update and rename dbo.SystemUserAssignedRoles.Table.sql to dbo.SysUserAssignedRoles.Table.sql

---
+++ db backups/dbo.SysUserAssignedRoles.Table.sql
@@ -0,0 +1,28 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[SysUserAssignedRoles]    Script Date: 2/11/2025 10:30:58 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SysUserAssignedRoles](
+	[Sys_user_assigned_role_id] [int] IDENTITY(1,1) NOT NULL,
+	[Sys_user_id] [int] NOT NULL,
+	[Sys_role_id] [int] NOT NULL,
+ CONSTRAINT [PK_SysUserAssignedRoles] PRIMARY KEY CLUSTERED 
+(
+	[Sys_user_assigned_role_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[SysUserAssignedRoles]  WITH CHECK ADD  CONSTRAINT [FK_SysUserAssignedRoles_Roles] FOREIGN KEY([Sys_role_id])
+REFERENCES [dbo].[SysRoles] ([Sys_role_id])
+GO
+ALTER TABLE [dbo].[SysUserAssignedRoles] CHECK CONSTRAINT [FK_SysUserAssignedRoles_Roles]
+GO
+ALTER TABLE [dbo].[SysUserAssignedRoles]  WITH CHECK ADD  CONSTRAINT [FK_SysUserAssignedRoles_SysUsers] FOREIGN KEY([Sys_user_id])
+REFERENCES [dbo].[SysUsers] ([Sys_user_id])
+GO
+ALTER TABLE [dbo].[SysUserAssignedRoles] CHECK CONSTRAINT [FK_SysUserAssignedRoles_SysUsers]
+GO
+
