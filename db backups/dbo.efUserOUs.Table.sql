8b00240d87c855ce7c3ca7e5bc38c32e16b67c22
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-08 14:54:00 -0400

    Update and rename dbo.SysUserOUs.Table.sql to dbo.efUserOUs.Table.sql

---
+++ db backups/dbo.efUserOUs.Table.sql
@@ -0,0 +1,25 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[efUserOUs]    Script Date: 2/11/2025 10:30:58 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[efUserOUs](
+	[ef_user_ou_id] [int] IDENTITY(1,1) NOT NULL,
+	[ou_name] [nvarchar](max) NULL,
+	[ef_user_id] [int] NOT NULL,
+	[created_at] [datetime2](7) NULL,
+ CONSTRAINT [PK_UserOUs] PRIMARY KEY CLUSTERED 
+(
+	[ef_user_ou_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[efUserOUs]  WITH CHECK ADD  CONSTRAINT [FK_efUserOUs] FOREIGN KEY([ef_user_id])
+REFERENCES [dbo].[efUsers] ([ef_user_id])
+GO
+ALTER TABLE [dbo].[efUserOUs] CHECK CONSTRAINT [FK_efUserOUs]
+GO
+
+
