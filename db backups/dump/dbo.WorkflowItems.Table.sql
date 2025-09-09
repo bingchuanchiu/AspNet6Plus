366b27083f8de3572dafc58b05bfc0f58e5f9b36
bingchuanchiu <bingchuanchiu@gmail.com>
2025-09-09 11:14:55 -0400

    Update and rename db backups/dbo.WorkflowItems.Table.sql to db backups/dump/dbo.WorkflowItems.Table.sql

---
+++ db backups/dump/dbo.WorkflowItems.Table.sql
@@ -0,0 +1,13 @@
+﻿USE [Complex]
+GO
+/****** Object:  Table [dbo].[WorkflowItems]    Script Date: 2/11/2025 3:42:30 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[WorkflowItems](
+	[workflow_item_id] [int] NOT NULL,
+	[name] [varchar](max) NOT NULL,
+	[detail] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
